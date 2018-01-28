2c9b904158a1ae305d6a0b8676a1883937b4dc5e
kwuenschel <kwuenschel@gmx.net>
2018-01-28 22:40:10 +0100

    First asyncio-version

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-import mp3, readerplayer, webplayer, time
+import mp3
+import readerplayer
+import uasyncio as asyncio
+import webplayer
 
 
 def main():
-    try:
-        mp3.set_volume(15)
-        web_player = webplayer.WebPlayer(mp3)
-        reader_player = readerplayer.ReaderPlayer(mp3)
-        while True:
-            web_player.do()
-            reader_player.do()
-    except Exception as e:
-        f = open("error.log", "w")
-        f.write(str(e))
-        f.close()
-        raise
+    mp3.set_volume(15)
+    loop = asyncio.get_event_loop()
+    webplayer.create_server()
+
+    reader_player = readerplayer.ReaderPlayer()
+    loop.create_task(reader_player.run())
+    loop.run_forever()
+
 
 if __name__ == '__main__':
     main()
-        
--- readerplayer.py
+++ readerplayer.py
@@ -1,5 +1,9 @@
-import mfrc522, rfidcards, time
-from machine import Pin
+import mp3
+import machine
+import mfrc522
+import rfidcards
+import time
+import uasyncio as asyncio
 
 DEBUG = False
 
@@ -20,16 +24,15 @@ class ReaderPlayer:
     LED_PIN = 15 #D8
     BUTTON_PIN = 12 #D6
     SPECIAL_CARDS = {
-        'end_program': '0xc0c92583',
+        'end_program': '0x65c08cb9',
         #'next_song': '0xc0125e7a',
         'next_unassigned_folder': '0x1206be59', #preview the next folder that is not yet assigned to a card, to 'program' the next tag
         }
     
-    def __init__(self, mp3Player):
-        self.led = Pin(self.LED_PIN, Pin.OUT)
-        self.button = Pin(self.BUTTON_PIN, Pin.IN, Pin.PULL_UP)
+    def __init__(self):
+        self.led = machine.Pin(self.LED_PIN, machine.Pin.OUT)
+        self.button = machine.Pin(self.BUTTON_PIN, machine.Pin.IN, machine.Pin.PULL_UP)
         self.button_last_pressed = time.time()
-        self.mp3Player = mp3Player
         self.status = None
         self.current_folder = None
         self.pause()
@@ -40,7 +43,7 @@ class ReaderPlayer:
         """ Is true, if a song just finished playing
              The mp3-player returns a specific code, after ending a song.
         """
-        uart_return_code = self.mp3Player.uart.readline()
+        uart_return_code = mp3.uart.readline()
         _debug("uart_return_code", uart_return_code)
         # uart_return_code == b'~\xff\x06=\x00\x00\x12\xfe\xac\xef~\xff\x06=\x00\x00\x12\xfe\xac\xef'
         return uart_return_code and b'\x06=' in uart_return_code 
@@ -75,67 +78,63 @@ class ReaderPlayer:
             # thrown from time to time from the reader...  no idea why.
             return
         
-    def _do(self):
-        if self.status == self.PLAYING:
-            if self.song_just_finished():
-                _debug("next")
-                self.mp3Player.next()
-            if not self.card_still_there():
-                self.pause()
-                #sometimes pause gets ignored by the player, so lets pause again, can't harm.
-                time.sleep(0.1)
-                self.pause()
-            else:
-                if not self.button.value():
-                    #button pressed
-                    if time.time() - self.button_last_pressed >= 2:
-                        #mind. 2 Sekunde seit letztem mal
-                        self.mp3Player.next()
-                        self.button_last_pressed = time.time()
-                return
+    async def run(self):
+        while True:
+            if self.status == self.PLAYING:
+                if self.song_just_finished():
+                    _debug("next")
+                    mp3.next()
+                if not self.card_still_there():
+                    self.pause()
+                    #sometimes pause gets ignored by the player, so lets pause again, can't harm.
+                    yield from asyncio.sleep_ms(100)
+                    self.pause()
+                else:
+                    if not self.button.value():
+                        #button pressed
+                        if time.time() - self.button_last_pressed >= 2:
+                            #mind. 2 Sekunde seit letztem mal
+                            mp3.next()
+                            self.button_last_pressed = time.time()
+            elif self.status == self.PAUSED:
+                #resume folder or play new one
+                #also check special cards
+                uid = self.get_card_id()
+                _debug("uid", uid)
+                if uid == self.SPECIAL_CARDS["end_program"]:
+                    mp3.stop()
+                    raise SystemExit('Stop program')
+                elif uid == self.SPECIAL_CARDS["next_unassigned_folder"]:
+                    self.play_folder(self.rfid_cards.next_card_index())
+                elif uid:
+                    folder_id = self.rfid_cards.card_index(uid)
+                    _debug("folder_id", folder_id)
+                    if folder_id == self.current_folder:
+                        _debug("same folder", folder_id)
+                        #user put same card back
+                        self.resume()
+                    else: #new card
+                        self.play_folder(folder_id)
+                        _debug("new card", folder_id)
 
-        elif self.status == self.PAUSED:
-            #resume folder or play new one
-            #also check special cards
-            uid = self.get_card_id()
-            _debug("uid", uid)
-            if not uid:
-                #still no card, still paused
-                #auskommentiert wegen webplayer
-                #self.pause() #sometimes pause gets ignored by the player, so lets pause again, can't harm.
-                return
-            if uid == self.SPECIAL_CARDS["end_program"]:
-                self.mp3Player.stop()
-                raise SystemExit('Stop program')
-            elif uid == self.SPECIAL_CARDS["next_unassigned_folder"]:
-                self.play_folder(self.rfid_cards.next_card_index())
-                return
-            folder_id = self.rfid_cards.card_index(uid)
-            _debug("folder_id", folder_id)
-            if folder_id == self.current_folder:
-                _debug("same folder", folder_id)
-                #user put same card back
-                self.resume()
-            else: #new card
-                self.play_folder(folder_id)
-                _debug("new card", folder_id)
-                
+            yield from asyncio.sleep_ms(100)
+
     def play_folder(self, folder_id):
         #folder_id starting from zero, mp3 player from one
-        self.mp3Player.play_folder(folder_id + 1)
+        mp3.play_folder(folder_id + 1)
         _debug("play_folder", folder_id)
         self.status = self.PLAYING
         self.led.on()
         self.current_folder = folder_id
         
     def resume(self):
-        self.mp3Player.resume()
+        mp3.resume()
         _debug("resume")
         self.status = self.PLAYING
         self.led.on()
         
     def pause(self):
-        self.mp3Player.pause()
+        mp3.pause()
         _debug("pause")
         self.status = self.PAUSED
         self.led.off()    
